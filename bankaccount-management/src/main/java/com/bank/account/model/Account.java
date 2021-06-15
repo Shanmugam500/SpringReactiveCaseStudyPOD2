daec54aa861f7881012364d97d1f602445562ca9
Shanmugam500 <32445669+Shanmugam500@users.noreply.github.com>
2021-06-15 13:45:32 +0530

    Add files via upload

--- bankaccount-management/src/main/java/com/bank/account/model/Account.java
+++ bankaccount-management/src/main/java/com/bank/account/model/Account.java
@@ -5,11 +5,11 @@ import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-@Data
 public class Account implements Serializable{
 	
+
 	private static final long serialVersionUID = 1530699992135610397L;
 
 	@Id
@@ -17,7 +17,9 @@ public class Account implements Serializable{
 	private String userid;
 	private String accounttype;
 	private double balance;
+	@JsonIgnore
 	private Date accountcreated;
+	@JsonIgnore
 	private Date accountupdated;
 	private String accountstatus;
 	private String accountbranch;
@@ -38,4 +40,69 @@ public class Account implements Serializable{
 		this.accountstatus = accountstatus;
 		this.accountbranch = accountbranch;
 	}
+	
+	public int getAccountno() {
+		return accountno;
+	}
+
+	public void setAccountno(int accountno) {
+		this.accountno = accountno;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getAccounttype() {
+		return accounttype;
+	}
+
+	public void setAccounttype(String accounttype) {
+		this.accounttype = accounttype;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Date getAccountcreated() {
+		return accountcreated;
+	}
+
+	public void setAccountcreated(Date accountcreated) {
+		this.accountcreated = accountcreated;
+	}
+
+	public Date getAccountupdated() {
+		return accountupdated;
+	}
+
+	public void setAccountupdated(Date accountupdated) {
+		this.accountupdated = accountupdated;
+	}
+
+	public String getAccountstatus() {
+		return accountstatus;
+	}
+
+	public void setAccountstatus(String accountstatus) {
+		this.accountstatus = accountstatus;
+	}
+
+	public String getAccountbranch() {
+		return accountbranch;
+	}
+
+	public void setAccountbranch(String accountbranch) {
+		this.accountbranch = accountbranch;
+	}
+	
 }
